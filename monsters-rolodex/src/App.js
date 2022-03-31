a9e1412aa76e1459225544aec486ee11cb2a41d6
shermaine <shermaine.lim.si.hui@gmail.com>
2022-03-31 13:00:09 +0800

    useEffect search field optimize

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -15,15 +16,17 @@ const App = () => {
       .then((users) => setMonsters(users));
   });
 
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
-
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchField);
-  });
-
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
