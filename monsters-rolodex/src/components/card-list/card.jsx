7d7ebdeaf4a78ab0904b20e4c51dfb1e79d8aa83
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-01 20:48:29 +0800

    code refactor to functional

--- monsters-rolodex/src/components/card-list/card.jsx
+++ monsters-rolodex/src/components/card-list/card.jsx
@@ -1,20 +1,16 @@
-import { Component } from "react";
 import "./card.css";
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monsters ${name}`}
-          src={`https://robohash.org/${id}?set=set2`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ id, name, email }) => {
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monsters ${name}`}
+        src={`https://robohash.org/${id}?set=set2`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
--- monsters-rolodex/src/components/card-list/cardList.jsx
+++ monsters-rolodex/src/components/card-list/cardList.jsx
@@ -1,19 +1,14 @@
-import { Component } from "react";
 import "./cardList.css";
 import Card from "./card";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
