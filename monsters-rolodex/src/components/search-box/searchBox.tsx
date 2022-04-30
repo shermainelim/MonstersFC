0bd639ba391efae5589d2d64088ca988dd191324
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-30 14:51:07 +0800

    Typescript convert

---
+++ monsters-rolodex/src/components/search-box/searchBox.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent } from "react";
+import "./searchBox.css";
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: SearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
